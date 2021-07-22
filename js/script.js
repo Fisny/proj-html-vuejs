0d3552c2e4d3d897427de31c7eef9c73117322f7
Fisny <bardi.fisniku@gmail.com>
2021-07-22 16:44:52 +0200

    search

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 new Vue({
     el: "#app",
     data: {
+        text: "",
         wishArray: [
             {
-                immagine: "1a.jpg",
+                immagine: "1a",
                 tipo: "Fresh Fruits",
                 nomeProdotto: "Mango Organic Fruit",
                 prezzo: "€18.55 - €48.55",
             },
             {
-                immagine: "2a.jpg",
+                immagine: "2a",
                 tipo: "Fresh Meat",
                 nomeProdotto: "Meat Veal Orloff Beef Steak",
                 prezzo: "€36.99 - €45.00",
             },
             {
-                immagine: "4a.jpg",
+                immagine: "4a",
                 tipo: "Fresh Vegetables",
                 nomeProdotto: "Chickpea Vegetable Legume Peas",
                 prezzo: "€32.00",
             },
             {
-                immagine: "5a.jpg",
+                immagine: "5a",
                 tipo: "Fresh Fruits",
                 nomeProdotto: "Pomegranate Fruit Peach",
                 prezzo: "€18.55 - €20.65",
@@ -31,7 +32,7 @@ new Vue({
     methods: {
         wishButton(elemento) {
 
-            let blocco = {
+            let blocco = { // c'è qualcosa di sbagliato
                 immagine: elemento, 
                 tipo: elemento, 
                 nomeProdotto: elemento, 
@@ -39,6 +40,14 @@ new Vue({
             }
             console.log(blocco)
             this.wishArray.push(blocco)
+
+            // se il blocco appare
+            // bisogna far comparire una 
+            // x al posto del pulsante wishlist
+        },
+        cerca () {
+            let testo = this.text
+            console.log("hai cercato " + testo)
         }
     },
 })
