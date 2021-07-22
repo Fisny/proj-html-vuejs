436bb25f59d816e3543dd19a19565fcfff403f18
Fisny <bardi.fisniku@gmail.com>
2021-07-22 13:32:22 +0200

    Objects wishlist

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 new Vue({
     el: "#app",
-    data: {},
+    data: {
+        wishArray: [
+            {
+                immagine: "1a.jpg",
+                tipo: "Fresh Fruits",
+                nomeProdotto: "Mango Organic Fruit",
+                prezzo: "€18.55 - €48.55",
+            },
+            {
+                immagine: "2a.jpg",
+                tipo: "Fresh Meat",
+                nomeProdotto: "Meat Veal Orloff Beef Steak",
+                prezzo: "€36.99 - €45.00",
+            },
+            {
+                immagine: "4a.jpg",
+                tipo: "Fresh Vegetables",
+                nomeProdotto: "Chickpea Vegetable Legume Peas",
+                prezzo: "€32.00",
+            },
+            {
+                immagine: "5a.jpg",
+                tipo: "Fresh Fruits",
+                nomeProdotto: "Pomegranate Fruit Peach",
+                prezzo: "€18.55 - €20.65",
+            }
+        ],
+    },
     methods: {},
 })
